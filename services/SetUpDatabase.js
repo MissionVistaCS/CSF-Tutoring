f47be2b319e50e6867634d107ef7b6788c490b0f
Andrew Yates <andrewmacu@me.com>
2018-05-10 21:16:00 -0700

    Cleaned routes, changed user model

--- services/SetUpDatabase.js
+++ services/SetUpDatabase.js
@@ -1,20 +1,20 @@
 const mongoose = require('mongoose');
 
-module.exports = function() {
-	const url = _db.get('db.connection.' + _env);
-	console.info('Attempting to cnnect to', url);
-	mongoose.connect(url);
-	mongoose.connection.on('connected', function() {
-		console.info('Database connection established');
-	});
+module.exports = function () {
+    const url = _db.get('db.connection.' + _env);
+    console.info('Attempting to cnnect to', url);
+    mongoose.connect(url);
+    mongoose.connection.on('connected', function () {
+        console.info('Database connection established');
+    });
 
-	mongoose.connection.on('error', function(err) {
-		console.critical('Cannot connect to database');
-		console.critical(JSON.stringify(err));
-		return process.exit();
-	});
+    mongoose.connection.on('error', function (err) {
+        console.critical('Cannot connect to database');
+        console.critical(JSON.stringify(err));
+        return process.exit();
+    });
 
-	mongoose.connection.on('disconnected', function() {
-		console.info('Database disconnected');
-	});
-}
+    mongoose.connection.on('disconnected', function () {
+        console.info('Database disconnected');
+    });
+};
