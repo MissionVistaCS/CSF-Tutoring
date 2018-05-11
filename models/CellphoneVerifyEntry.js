71ac2a73abc9aa9a0413b51c1e8153d6094b90d8
Andrew Yates <andrewmacu@me.com>
2018-05-10 21:27:40 -0700

    Name scheme change + verify entry

---
+++ models/CellphoneVerifyEntry.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose'),
+    shortid = require('shortid');
+
+let cellPhoneSchema = new mongoose.Schema({
+    _id: { type: String, default: shortid.generate },
+    user: [{ type: String, ref: 'User' }]
+}, { collection: _db.get('db.collection.cellphone-verify-entry') });
+
+let CellphoneVerifyEntry = mongoose.model('CellphoneVerifyEntry', cellPhoneSchema);
+
+module.exports = CellphoneVerifyEntry;
--- models/user.js
+++ models/user.js
@@ -1,24 +1,24 @@
 const mongoose = require('mongoose'),
-	shortid = require('shortid');
+    shortid = require('shortid');
 
 let userSchema = new mongoose.Schema({
-	_id: { type: String, default: shortid.generate },
-	fullName: { type: String, required: true },
-	password: { type: String, required: true },
-	gender: { type: String, required: true },
-	grade: { type: Number, required: true },
-	email: { type: String, required: true },
-	cellPhoneNum: { type: String, required: true },
-	userGroup: { type: [String], required: true },
-	created: { type: Date, required: true, default: Date.now },
-	active: { type: Boolean, required: true, default: true },
-	verified: { type: Boolean, required: true, default: false },
-	cellPhoneVerified: { type: Boolean, required: true, default: false },
-	warnings: { type: Number, required: true, default: 0 },
-	// if a tutor
-	maxStudents: { type: Number, required: false },
-	payment: { type: String, required: false },
-	courses: { type: [Number], required: false }
+    _id: { type: String, default: shortid.generate },
+    fullName: { type: String, required: true },
+    password: { type: String, required: true },
+    gender: { type: String, required: true },
+    grade: { type: Number, required: true },
+    email: { type: String, required: true },
+    cellPhoneNum: { type: String, required: true },
+    userGroup: { type: [String], required: true },
+    created: { type: Date, required: true, default: Date.now },
+    active: { type: Boolean, required: true, default: true },
+    verified: { type: Boolean, required: true, default: false },
+    cellPhoneVerified: { type: Boolean, required: true, default: false },
+    warnings: { type: Number, required: true, default: 0 },
+    // if a tutor
+    maxStudents: { type: Number, required: false },
+    payment: { type: String, required: false },
+    courses: { type: [Number], required: false }
 }, { collection: _db.get('db.collection.users') });
 
 let User = mongoose.model('User', userSchema);
