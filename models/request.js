66ef21d7ab8723d409ee8da446b2621a90359fdf
Andrew Yates <andrewmacu@me.com>
2018-05-10 21:19:43 -0700

    Change request model

--- models/request.js
+++ models/request.js
@@ -3,22 +3,22 @@ const mongoose = require('mongoose'),
 
 let requestSchema = new mongoose.Schema({
 	_id: { type: String, default: shortid.generate },
-	tutor: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }],
-	studentName: { type: String, required: true },
-    studentEmail: { type: String, required: true },
-	studentPhone: { type: String, required: true },
-	grade: { type: Number, required: true },
+	tutor: [{ type: String, ref: 'User' }],
+	fullName: { type: String, required: true },
 	gender: { type: String, required: true },
-	parentName: { type: String, required: true },
+	grade: { type: Number, required: true },
+	email: { type: String, required: true },
+	cellPhoneNum: { type: String, required: true },
+	parentFullName: { type: String, required: true },
 	parentEmail: { type: String, required: true },
-	parentPhone: { type: String, required: true },
+	parentCellPhoneNum: { type: String, required: true },
 	payment: { type: String, required: true }, 
 	duplicate: { type: Boolean, required: true },
 	courses: { type: [Number], required: true },
-	created: { type: String, required: true, default: Date.now },
+	created: { type: Date, required: true, default: Date.now },
 	state: { type: String, required: true }, 
 	notifications: { type: [Date], required: true },
-	pairingAcceptance: { type: Date, required: true }
+	pairingAcceptance: { type: Date, required: false }
 }, { collection: _db.get('db.collection.requests') });
 
 let Request = mongoose.model('Request', requestSchema);
