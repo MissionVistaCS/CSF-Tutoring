53daaf72be8a9d6cdd717790e0a0d633fc379f4c
vwheezy22 <vinwhiz@gmail.com>
2018-05-23 13:02:19 -0700

    starting Vue

--- src/main.js
+++ src/main.js
@@ -5,16 +5,8 @@ import App from './App.vue'
 Vue.config.productionTip = false
 
 new Vue({
-<<<<<<< Updated upstream
-    el: '#app',
-    router,
-    template: '<App/>',
-    component: {App}
-});
-=======
   el: '#app',
   router,
   template: '<App/>',
   components: { App }
 })
->>>>>>> Stashed changes
