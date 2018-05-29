9d7ec2cab96e689a17390c08ec2d6bea7d39fbde
Ryan Keegan <r.keegan@me.com>
2018-05-28 23:58:42 -0700

    Biggest commit ever

--- src/components/ListUsers.vue
+++ src/components/ListUsers.vue
@@ -182,7 +182,7 @@
                 });
             },
             editRequest(entry) {
-                window.location.href = "/editUser/"+entry._id;
+                window.location.href = "/edituser/"+entry._id;
             }
         }
     }
